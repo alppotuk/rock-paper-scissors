cdef7233998bbed0b6e346a612ddddf4cde4c8e7
alppotuk <alppotuk@hotmail.com>
2023-04-03 17:03:56 +0300

    started project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GameComponent } from './pages/game/game.component';
+import { GameCardComponent } from './components/game-card/game-card.component';
+import { PlayerCardComponent } from './components/player-card/player-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    GameComponent,
+    GameCardComponent,
+    PlayerCardComponent
   ],
   imports: [
     BrowserModule,
